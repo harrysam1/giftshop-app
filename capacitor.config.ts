f87bf0354a0feddd6a500486ed81eb0af179628c
harrysam1 <157618456+harrysam1@users.noreply.github.com>
2025-05-02 09:55:56 +0100

    gift-shop-ngmodule
    plays a central role in organizing your app into cohesive blocks of functionality

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'gift-shop',
+  appName: 'gift-shop-ngmodule',
   webDir: 'www'
 };
 
